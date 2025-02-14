b25a7b4cf56d5cacf210cc7b81ff3d58e3774643
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2025-02-14 19:10:52 +0200

    create files and views of auth features

---
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUpViewBody extends StatelessWidget {
+  const SignUpViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
